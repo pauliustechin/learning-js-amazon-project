18b23d168f1933593ddd9f59dbdaea72087473c9
pauliussem <pauliustechin@gmail.com>
2025-10-24 12:21:27 +0300

    save data and generate HTML delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,25 @@
+// saving to local storage 3 zingsnis
+
 export let cart = JSON.parse(localStorage.getItem('cart'));
+
+// jei nera cart local storage 4 zingsnis
 
 if (!cart) {
   cart = [
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantyti: 2,
+      deliveryOptionId: '1'
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantyti: 1,
+      deliveryOptionId: '2'
     }
   ];
 }
 
+// saving cart to local storage 1 zingsnis
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -32,9 +39,13 @@ export function addToCart(productId) {
   } else {
     cart.push({
       productId: productId,
-      quantyti: 1
+      quantyti: 1,
+      deliveryOptionId: '1'
     });
   }
+
+
+  // pritaikom funkcija po cart pokycio 2 zingsnis  
 
   saveToStorage();
 }
@@ -50,5 +61,8 @@ export function removeFromCart(productId) {
   })
 
   cart = newCart;
+
+  // pritaikom funkcija po cart pokycio 2 zingsnis 
+
   saveToStorage();
 }
