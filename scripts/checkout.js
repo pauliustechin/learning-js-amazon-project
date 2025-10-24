f5533e37a4017f622925043721b04c52705f4aba
pauliussem <pauliustechin@gmail.com>
2025-10-24 13:15:22 +0300

    regenerate html when updating delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -22,96 +22,41 @@ deliveryDate.format('dddd, MMMM D')
 // console.log(deliveryDate.format('dddd, MMMM D'));
 
 
-let cartSummaryHTML = '';
-let radioNumber = 0;
-
-cart.forEach((cartItem) => {
-
-    const productId = cartItem.productId;
-
-    let matchingProduct;
-
-    //vietoj radio input number, galima naudoti tiesiog product ID.
-    radioNumber += 1;
-
-    products.forEach((product) => {
-        if (product.id === productId) {
-            matchingProduct = product;
-        }
-
-        // console.log(matchingProduct);
-    });
+function renderOrderSummary() {
 
 
-    const deliveryOptionId = cartItem.
-        deliveryOptionId;
 
-    let deliveryOption;
+    let cartSummaryHTML = '';
+    let radioNumber = 0;
 
-    deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-            deliveryOption = option;
-        }
-    });
+    cart.forEach((cartItem) => {
 
-    const today = dayjs();
-    const deliveryDate = today.add(
-        deliveryOption.deliveryDays, 'days'
-    );
+        const productId = cartItem.productId;
 
-    const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-    );
+        let matchingProduct;
 
-    cartSummaryHTML += `
-        <div class="cart-item-container 
-        js-cart-item-container-${matchingProduct.id}">
-            <div class="delivery-date">
-                Delivery date: ${dateString}
-            </div>
+        //vietoj radio input number, galima naudoti tiesiog product ID.
+        radioNumber += 1;
 
-            <div class="cart-item-details-grid">
-                <img class="product-image"
-                src="${matchingProduct.image}">
+        products.forEach((product) => {
+            if (product.id === productId) {
+                matchingProduct = product;
+            }
 
-                <div class="cart-item-details">
-                <div class="product-name">
-                    ${matchingProduct.name}
-                </div>
-                <div class="product-price">
-                    $${formatCurrency(matchingProduct.priceCents)}
-                </div>
-                <div class="product-quantity">
-                    <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantyti}</span>
-                    </span>
-                    <span class="update-quantity-link link-primary">
-                    Update
-                    </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" 
-                    data-product-id="${matchingProduct.id}">
-                    Delete
-                    </span>
-                </div>
-                </div>
+            // console.log(matchingProduct);
+        });
 
-                <div class="delivery-options">
-                <div class="delivery-options-title">
-                    Choose a delivery option:
-                </div>
-                ${deliveryOptionsHTML(matchingProduct, cartItem)}
-               
-                </div>
-            </div>
-        </div>
-        `
-});
 
-function deliveryOptionsHTML(matchingProduct, cartItem) {
+        const deliveryOptionId = cartItem.
+            deliveryOptionId;
 
-    let html = '';
+        let deliveryOption;
 
-    deliveryOptions.forEach((deliveryOption) => {
+        deliveryOptions.forEach((option) => {
+            if (option.id === deliveryOptionId) {
+                deliveryOption = option;
+            }
+        });
 
         const today = dayjs();
         const deliveryDate = today.add(
@@ -122,62 +67,125 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
             'dddd, MMMM D'
         );
 
-        // kai norime if statement padaryti variable >>> ? if true : if false
-        const priceString = deliveryOption === 0 ? 'FREE'
-            : `$${formatCurrency(deliveryOption.priceCents)} -`;
+        cartSummaryHTML += `
+            <div class="cart-item-container 
+            js-cart-item-container-${matchingProduct.id}">
+                <div class="delivery-date">
+                    Delivery date: ${dateString}
+                </div>
 
-        // tam, kad nebutu checked visi radio buttons.
-        const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+                <div class="cart-item-details-grid">
+                    <img class="product-image"
+                    src="${matchingProduct.image}">
 
-        html +=
+                    <div class="cart-item-details">
+                    <div class="product-name">
+                        ${matchingProduct.name}
+                    </div>
+                    <div class="product-price">
+                        $${formatCurrency(matchingProduct.priceCents)}
+                    </div>
+                    <div class="product-quantity">
+                        <span>
+                        Quantity: <span class="quantity-label">${cartItem.quantyti}</span>
+                        </span>
+                        <span class="update-quantity-link link-primary">
+                        Update
+                        </span>
+                        <span class="delete-quantity-link link-primary js-delete-link" 
+                        data-product-id="${matchingProduct.id}">
+                        Delete
+                        </span>
+                    </div>
+                    </div>
+
+                    <div class="delivery-options">
+                    <div class="delivery-options-title">
+                        Choose a delivery option:
+                    </div>
+                    ${deliveryOptionsHTML(matchingProduct, cartItem)}
+                
+                    </div>
+                </div>
+            </div>
             `
-        <div class="delivery-option js-delivery-option"
-            data-product-id="${matchingProduct.id}"
-            data-delivery-option-id="${deliveryOption.id}"
-            >
-            <input type="radio" 
-            ${isChecked ? 'checked' : ''}
-            class="delivery-option-input"
-            // //////////////////////////////
-            name="delivery-option-${matchingProduct.id}">
-            <div>
-            <div class="delivery-option-date">
-                ${dateString}
-            </div>
-            <div class="delivery-option-price">
-                ${priceString} Shipping
-            </div>
-            </div>
-        </div>
-        `
     });
-    return html;
-};
 
-document.querySelector('.js-order-summary')
-    .innerHTML = cartSummaryHTML;
+    function deliveryOptionsHTML(matchingProduct, cartItem) {
 
-document.querySelectorAll('.js-delete-link')
-    .forEach((link) => {
-        link.addEventListener('click', () => {
-            //    console.log('delete') 
+        let html = '';
 
-            const productId = link.dataset.productId;
-            // console.log(link.dataset.productId);
-            removeFromCart(productId);
-            // console.log(cart);
+        deliveryOptions.forEach((deliveryOption) => {
 
-            const container = document.querySelector(`.js-cart-item-container-${productId}`);
-            // console.log(container);
+            const today = dayjs();
+            const deliveryDate = today.add(
+                deliveryOption.deliveryDays, 'days'
+            );
 
-            container.remove();
+            const dateString = deliveryDate.format(
+                'dddd, MMMM D'
+            );
+
+            // kai norime if statement padaryti variable >>> ? if true : if false
+            const priceString = deliveryOption === 0 ? 'FREE'
+                : `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+            // tam, kad nebutu checked visi radio buttons.
+            const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+            html +=
+                `
+            <div class="delivery-option js-delivery-option"
+                data-product-id="${matchingProduct.id}"
+                data-delivery-option-id="${deliveryOption.id}"
+                >
+                <input type="radio" 
+                ${isChecked ? 'checked' : ''}
+                class="delivery-option-input"
+                // //////////////////////////////
+                name="delivery-option-${matchingProduct.id}">
+                <div>
+                <div class="delivery-option-date">
+                    ${dateString}
+                </div>
+                <div class="delivery-option-price">
+                    ${priceString} Shipping
+                </div>
+                </div>
+            </div>
+            `
         });
-    });
+        return html;
+    };
+
+    document.querySelector('.js-order-summary')
+        .innerHTML = cartSummaryHTML;
+
+    document.querySelectorAll('.js-delete-link')
+        .forEach((link) => {
+            link.addEventListener('click', () => {
+                //    console.log('delete') 
+
+                const productId = link.dataset.productId;
+                // console.log(link.dataset.productId);
+                removeFromCart(productId);
+                // console.log(cart);
+
+                const container = document.querySelector(`.js-cart-item-container-${productId}`);
+                // console.log(container);
+
+                container.remove();
+            });
+        });
 
     document.querySelectorAll('.js-delivery-option')
         .forEach((element) => {
             element.addEventListener('click', () => {
-                const {productId, deliveryOptionId} = element.dataset;
+                const { productId, deliveryOptionId } = element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId);
+                renderOrderSummary();
             });
         });
+};
+
+renderOrderSummary();
