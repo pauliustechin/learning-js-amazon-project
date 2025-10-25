772ae1303039d92581e39a5de91be97589265284
pauliussem <pauliustechin@gmail.com>
2025-10-25 17:31:07 +0300

    learn more details about classes and  this

--- data/products.js
+++ data/products.js
@@ -103,6 +103,40 @@ class Clothing extends Product {
 
 // console.log(product1);
 
+
+// built-in classes:
+// const date = new Date();
+// console.log(date);
+
+// console.log(date.toLocaleTimeString());
+
+
+// console.log(this);
+
+// const object2 = {
+//   a: 2,
+//   b: this.a,
+// }
+
+// function logThis() {
+//   console.log(this);
+// }
+
+// logThis();
+
+// // bus console.log = hello
+// logThis.call('hello');
+
+// const object3 = {
+//   method: () => {
+//     console.log(this)
+//   }
+// }
+
+// object3.method();
+
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
