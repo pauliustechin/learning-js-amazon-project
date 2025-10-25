5b773a22fad78dc70dcedd6f54afef39021f3f73
pauliussem <pauliustechin@gmail.com>
2025-10-25 20:54:26 +0300

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,9 +3,77 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practise.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(() => {
+new Promise((resolve) => {
+    // console.log('start promise')
+    loadProducts(() => {
+        // console.log('finished loading')
+        resolve();
+    });
+}).then(() => {
+    // console.log('next step');
     renderOrderSummary();
     renderPaymentSummary();
 });
 
+
+
+// CALL BACK
+// loadProducts(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
+
+// CALL BACK cart
+// loadProducts(() => {
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
+
+
+// // kai baigia visus promise, tada juda i kita zingsni then()
+// // taip galima  vienu metu daug promise vykdyti o ne viena po kito
+// Promise.all([
+
+//     new Promise((resolve) => {
+//         loadProducts(() => {
+//             resolve('value1');
+//         });
+//     }),
+
+//     new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//     })
+
+// ]).then((values) => {
+//     console.log(values);
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
+
+
+
+// // PROMISE cart
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         //galime duoti value resolve ir naudoti kitame zingsnyje
+//         resolve('value1');
+//     });
+
+// }).then((value) => {
+//     console.log(value)
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//     });
+
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
